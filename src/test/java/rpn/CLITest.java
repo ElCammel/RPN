d4311758bd74e580bea74408e2bc2c384dbfaa01
Pinault <pinben02@gmail.com>
2019-05-12 15:52:53 +0200

    Update CLITest.java

--- src/test/java/rpn/CLITest.java
+++ src/test/java/rpn/CLITest.java
@@ -26,4 +26,35 @@ public class CLITest {
     public void should_evaluate_more_complex_addition() {
         assertThat(evaluate("2 3 5 + +")).isEqualTo(10);
     }
+    
+        @Test
+    public void should_evaluate_simple_substraction() {
+        assertThat(evaluate("17 5 -")).isEqualTo(12);
+    }
+
+    @Test
+    public void should_evaluate_more_complex_substraction() {
+        assertThat(evaluate("2 3 5 - -")).isEqualTo(-6);
+    }
+    
+            @Test
+    public void should_evaluate_simple_mutliplication() {
+        assertThat(evaluate("17 5 *")).isEqualTo(85);
+    }
+
+    @Test
+    public void should_evaluate_more_complex_multiplication() {
+        assertThat(evaluate("2 3 5 * *")).isEqualTo(30);
+    }
+    
+            @Test
+    public void should_evaluate_simple_division() {
+        assertThat(evaluate("17 5 /")).isEqualTo(3.4);
+    }
+
+    @Test
+    public void should_evaluate_0_divided() {
+        assertThat(evaluate("2 0 /")).isEqualTo(0);
+    }
+    
 }
